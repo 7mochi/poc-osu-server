29f349524be2cc8608fda3540ed1f97fab4129c0
7mochi <flyingcatdm@gmail.com>
2026-01-28 23:19:16 -0500

    feat: implement seasonal backgrounds

---
+++ src/main/java/pe/nanamochi/banchus/controllers/SeasonalController.java
@@ -0,0 +1,20 @@
+package pe.nanamochi.banchus.controllers;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/web")
+public class SeasonalController {
+
+  @Value("${banchus.seasonal-backgrounds.urls}")
+  private List<String> seasonalBackgroundsUrls;
+
+  @GetMapping(value = "/osu-getseasonal.php")
+  public List<String> seasonalBackgrounds() {
+    return seasonalBackgroundsUrls;
+  }
+}

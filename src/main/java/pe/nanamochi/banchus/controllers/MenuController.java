7c5e59af4bbd114a1cecd452aff9ba4ea34ac8d2
7mochi <flyingcatdm@gmail.com>
2026-01-28 21:09:56 -0500

    chore: simplify Caddyfile

--- src/main/java/pe/nanamochi/banchus/controllers/MenuController.java
+++ src/main/java/pe/nanamochi/banchus/controllers/MenuController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/menu")
+@RequestMapping("/")
 public class MenuController {
 
   @Value("${banchus.menu-icon.image-url}")

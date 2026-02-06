0a3ae822ea9ef511a9e978f06a6f59b524f4dacb
7mochi <flyingcatdm@gmail.com>
2026-02-06 14:51:12 -0500

    feat: handle multiplayer state on client exit

--- src/main/java/pe/nanamochi/banchus/packets/client/handlers/ExitPacketHandler.java
+++ src/main/java/pe/nanamochi/banchus/packets/client/handlers/ExitPacketHandler.java
@@ -4,15 +4,21 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.time.Duration;
 import java.time.Instant;
+import java.util.List;
 import java.util.Set;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+import pe.nanamochi.banchus.entities.Mods;
 import pe.nanamochi.banchus.entities.QuitState;
+import pe.nanamochi.banchus.entities.SlotStatus;
+import pe.nanamochi.banchus.entities.SlotTeam;
 import pe.nanamochi.banchus.entities.db.Channel;
 import pe.nanamochi.banchus.entities.db.Session;
+import pe.nanamochi.banchus.entities.redis.MultiplayerMatch;
+import pe.nanamochi.banchus.entities.redis.MultiplayerSlot;
 import pe.nanamochi.banchus.entities.redis.PacketBundle;
 import pe.nanamochi.banchus.packets.AbstractPacketHandler;
 import pe.nanamochi.banchus.packets.PacketWriter;
@@ -20,6 +26,7 @@ import pe.nanamochi.banchus.packets.Packets;
 import pe.nanamochi.banchus.packets.client.ExitPacket;
 import pe.nanamochi.banchus.packets.server.ChannelAvailablePacket;
 import pe.nanamochi.banchus.packets.server.ChannelRevokedPacket;
+import pe.nanamochi.banchus.packets.server.MatchDisbandPacket;
 import pe.nanamochi.banchus.packets.server.UserQuitPacket;
 import pe.nanamochi.banchus.services.*;
 
@@ -35,6 +42,8 @@ public class ExitPacketHandler extends AbstractPacketHandler<ExitPacket> {
   private final ChannelService channelService;
   private final ChannelMembersService channelMembersService;
   private final SpectatorService spectatorService;
+  private final MultiplayerService multiplayerService;
+  private final MatchBroadcastService matchBroadcastService;
 
   @Override
   public Packets getPacketType() {
@@ -91,7 +100,60 @@ public class ExitPacketHandler extends AbstractPacketHandler<ExitPacket> {
       }
     }
 
-    // TODO: Multiplayer
+    // Handle the player being in a multiplayer match
+    if (session.getMultiplayerMatchId() != null) {
+      MultiplayerMatch match = multiplayerService.findById(session.getMultiplayerMatchId());
+
+      // Fetch our slot
+      MultiplayerSlot slot =
+          multiplayerService.findSlotBySessionId(match.getMatchId(), session.getId());
+
+      // Remove us from the match
+      slot.setUserId(-1);
+      slot.setSessionId(null);
+      slot.setStatus(SlotStatus.OPEN.getValue());
+      slot.setTeam(SlotTeam.NEUTRAL);
+      slot.setMods(Mods.NO_MOD.getValue());
+      slot.setLoaded(false);
+      slot.setSkipped(false);
+      multiplayerService.updateSlot(match.getMatchId(), slot);
+
+      Channel matchChannel = channelService.findByName("#mp_" + match.getMatchId());
+      if (match.getHostUserId() == session.getUser().getId()) {
+        // If the host left, pick a new host
+        List<MultiplayerSlot> slots = multiplayerService.getAllSlots(match.getMatchId());
+
+        MultiplayerSlot newHostSlot = null;
+        for (MultiplayerSlot s : slots) {
+          // Slot doesn't have a user
+          if (slot.getUserId() == -1) continue;
+          newHostSlot = s;
+          break;
+        }
+
+        // No one is left in the match, close it
+        if (newHostSlot == null) {
+          ByteArrayOutputStream stream = new ByteArrayOutputStream();
+          packetWriter.writePacket(stream, new MatchDisbandPacket(match.getMatchId()));
+          matchBroadcastService.broadcastToLobby(stream.toByteArray());
+
+          channelMembersService.removeMemberFromChannel(matchChannel, session);
+          stream = new ByteArrayOutputStream();
+          packetWriter.writePacket(stream, new MatchDisbandPacket(match.getMatchId()));
+          packetWriter.writePacket(stream, new ChannelRevokedPacket("#multiplayer"));
+          packetBundleService.enqueue(session.getId(), new PacketBundle(stream.toByteArray()));
+
+          // Delete the multiplayer match (and it's slots)
+          match = multiplayerService.delete(match.getMatchId());
+
+          // Delete the #multiplayer channel
+          matchChannel = channelService.delete(matchChannel);
+
+          logger.info(
+              "Multiplayer match {} closed due to no remaining players.", match.getMatchId());
+        }
+      }
+    }
 
     // Handle the player being in any chat channels
     for (Channel channel : channelService.getAllChannels()) {
